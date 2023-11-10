dc1a05926eff759eb320124fc459257310321175
s.godarzi <goudarzi.shahrazd825@gmail.com>
2023-11-10 12:56:27 +0330

    add drf_spectacular views

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 from blog.views import PostListAPIView
 
 urlpatterns = [
     path('post-list/', PostListAPIView.as_view(), name='post_list'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
 
 ]
