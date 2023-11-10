25ff56e5e768c44a4bce7a0f5f0f40bf87b2f945
s.godarzi <goudarzi.shahrazd825@gmail.com>
2023-11-10 11:59:53 +0330

    define Post model

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+
+
+class Post(models.Model):
+
+    title   = models.CharField(max_length=100, null=False, blank=False)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
