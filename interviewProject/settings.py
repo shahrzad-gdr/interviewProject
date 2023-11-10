3ae4bbb89231542e9fd8c94bd359e6cf230a32ab
s.godarzi <goudarzi.shahrazd825@gmail.com>
2023-11-10 12:57:22 +0330

    add drf_spectacular app to INSTALLED_APPS,

--- interviewProject/settings.py
+++ interviewProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'account',
     'blog',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
